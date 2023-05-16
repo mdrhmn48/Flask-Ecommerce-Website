0c0475610fedcd3e517a00b4d71424812ba37adb
Justin Weible <jweible23@gmail.com>
2023-05-16 11:07:13 -0400

    moved database files to new folder, created requirements.txt and .in

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from product_database import get_products, y, max_price, product_category
+from database_source_files.product_database import get_products, y, max_price, product_category
 from flask_login import  login_required, login_user, current_user
 
 views = Blueprint("views", __name__)
@@ -8,6 +8,3 @@ views = Blueprint("views", __name__)
 # @login_required
 def home():
     return render_template("home.html", title = 'Home', result=get_products("Vegetables"), foo=y, max=max_price(), prod_cat=product_category(), currentUser=current_user)
-
-
-
