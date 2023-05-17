1264e2a1c2bb2c9ff90a0262ff187fdb4a082fa5
Justin Weible <jweible23@gmail.com>
2023-05-17 10:42:30 -0400

    updated folder structure, created db

--- website/views.py
+++ website/views.py
@@ -1,5 +1,5 @@
 from flask import Blueprint, render_template
-from database_source_files.product_database import get_products, y, max_price, product_category
+from database_source_files.product_database import get_products
 from flask_login import  login_required, login_user, current_user
 
 views = Blueprint("views", __name__)
