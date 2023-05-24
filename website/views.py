1b9d004e47e0287b0de26e6284a787378e16b18b
mdrhmn48 <mdrhmn48@gmail.com>
2023-05-23 23:10:25 -0400

    n/a

--- website/views.py
+++ website/views.py
@@ -2,9 +2,9 @@ from flask import Blueprint, render_template
 from database_source_files.product_database import get_products
 from flask_login import  login_required, login_user, current_user
 
-views = Blueprint("views", __name__)
 
+views = Blueprint("views", __name__)
+# @login_required
 @views.route("/", methods= ["GET", "POST"])
-@login_required
 def home():
     return render_template("home.html", currentUser=current_user)
